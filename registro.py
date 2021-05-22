82fb52f4c191fdb7c5476c930a8c56791d965130
MiguelDelCastillo <47579615+MiguelDelCastillo@users.noreply.github.com>
2021-05-21 19:45:46 -0500

    Actualización del Codigo hecho

--- registro.py
+++ registro.py
@@ -12,6 +12,20 @@ class Participante(Participante):
 def muestra_datos():
     pass
 
+def DatosRestantes():
+    Nombre=input("Ingrese su nombre: ")
+    FechaNacimiento=input("Ingrese su Fecha de Nacimiento: ")
+    MomentoRegistro=datetime.datetime.now()
+
+def ProporcionarDatos():
+    global Correo
+    Correo=input("Ingrese su correo electronico: ")
+    Nombre=input("Ingrese su nombre: ")
+    FechaNacimiento=input("Ingrese su Fecha de Nacimiento: ")
+    MomentoRegistro=datetime.datetime.now()
+
+ProporcionarDatos()
+
 # RUTA DONDE SE GUARDARA EL ARCHIVO
 ruta=os.path.abspath(os.getcwd())
 
@@ -24,14 +38,31 @@ def carga_datos_csv():
     # Escribo la primer línea del archivo, con los encabezados.
     f.write(f"CORREO|NOMBRE|FECHA DE NACIMIENTO|MOMENTO DE REGISTRO\n")
     for elemento in Participantes:
-    f.write(f'{elemento.Correo}|{elemento.Nombre}|{elemento.FechaNacimiento}|{elemento.MomentoRegistro}\n')
+        f.write(f'{elemento.Correo}|{elemento.Nombre}|{elemento.FechaNacimiento}|{elemento.MomentoRegistro}\n')
     f.close()
 
 def registroParticipantes():
-    pass
+    global Ciclo
+    Ciclo=False
+    while (Ciclo==False):
+        RegistrarCorreo=input("Ingrese su correo electronico")
+        if RegistrarCorreo=="":
+            Ciclo=True
+        elif RegistrarCorreo == Correo:
+            print("Ese correo ya está registrado")
+            Ciclo=True
+        else:
+            DatosRestantes()
 
 def buscarParticipantes():
-    pass
+    while (Ciclo==False):
+        BuscarCorreo=input("Ingrese su correo electronico")
+        if BuscarCorreo=="":
+            Ciclo=True
+        elif BuscarCorreo == Correo:
+            print("Ese correo ya está registrado")
+            Ciclo=True
+            print
 
 def modParticipantes():
     pass
